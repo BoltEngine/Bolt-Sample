1ea75cb94cb3a8a61e30822b5242ad620d1bf4d0
Ramon Melo <me@ramonmelo.com>
2018-12-17 16:07:30 -0300

    CHANGED: Code formatting

--- PhotonCloud/demo_simple/Scripts/PhotonTokens.cs
+++ PhotonCloud/demo_simple/Scripts/PhotonTokens.cs
@@ -3,51 +3,51 @@ using System;
 
 namespace Photon
 {
-  public class RoomProtocolToken : Bolt.IProtocolToken
-  {
-    public String ArbitraryData;
-    public String password;
+	public class RoomProtocolToken : Bolt.IProtocolToken
+	{
+		public String ArbitraryData;
+		public String password;
 
-    public void Read(UdpPacket packet)
-    {
-      ArbitraryData = packet.ReadString();
-      password = packet.ReadString();
-    }
+		public void Read(UdpPacket packet)
+		{
+			ArbitraryData = packet.ReadString();
+			password = packet.ReadString();
+		}
 
-    public void Write(UdpPacket packet)
-    {
-      packet.WriteString(ArbitraryData);
-      packet.WriteString(password);
-    }
-  }
+		public void Write(UdpPacket packet)
+		{
+			packet.WriteString(ArbitraryData);
+			packet.WriteString(password);
+		}
+	}
 
-  public class ServerAcceptToken : Bolt.IProtocolToken
-  {
-    public String data;
+	public class ServerAcceptToken : Bolt.IProtocolToken
+	{
+		public String data;
 
-    public void Read(UdpPacket packet)
-    {
-      data = packet.ReadString();
-    }
+		public void Read(UdpPacket packet)
+		{
+			data = packet.ReadString();
+		}
 
-    public void Write(UdpPacket packet)
-    {
-      packet.WriteString(data);
-    }
-  }
+		public void Write(UdpPacket packet)
+		{
+			packet.WriteString(data);
+		}
+	}
 
-  public class ServerConnectToken : Bolt.IProtocolToken
-  {
-    public String data;
+	public class ServerConnectToken : Bolt.IProtocolToken
+	{
+		public String data;
 
-    public void Read(UdpPacket packet)
-    {
-      data = packet.ReadString();
-    }
+		public void Read(UdpPacket packet)
+		{
+			data = packet.ReadString();
+		}
 
-    public void Write(UdpPacket packet)
-    {
-      packet.WriteString(data);
-    }
-  }
+		public void Write(UdpPacket packet)
+		{
+			packet.WriteString(data);
+		}
+	}
 }

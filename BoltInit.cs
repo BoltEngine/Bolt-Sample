61fbd14ccde30f33292478d2309df13d3b95badd
Ramon Melo <me@ramonmelo.com>
2018-12-17 14:49:57 -0300

    CHANGED: Code formatting

--- BoltInit.cs
+++ BoltInit.cs
@@ -6,23 +6,23 @@ using udpkit.platform.photon.photon;
 
 namespace Bolt.Samples
 {
-    public class BoltInit : Bolt.GlobalEventListener
-    {
-        enum State
-        {
-            SelectMode,
-            SelectMap,
-            SelectRoom,
-            StartServer,
-            StartClient,
-            Started,
-        }
+	public class BoltInit : Bolt.GlobalEventListener
+	{
+		enum State
+		{
+			SelectMode,
+			SelectMap,
+			SelectRoom,
+			StartServer,
+			StartClient,
+			Started,
+		}
 
 		Rect labelRoom = new Rect(0, 0, 140, 75);
 		GUIStyle labelRoomStyle;
 
 		State state;
-        string map;
+		string map;
 
 		void Awake()
 		{
@@ -39,29 +39,29 @@ namespace Bolt.Samples
 			};
 		}
 
-        void OnGUI()
-        {
-            Rect tex = new Rect(10, 10, 140, 75);
-            Rect area = new Rect(10, 90, Screen.width - 20, Screen.height - 100);
+		void OnGUI()
+		{
+			Rect tex = new Rect(10, 10, 140, 75);
+			Rect area = new Rect(10, 90, Screen.width - 20, Screen.height - 100);
 
-            GUI.Box(tex, Resources.Load("BoltLogo") as Texture2D);
+			GUI.Box(tex, Resources.Load("BoltLogo") as Texture2D);
 
 			GUILayout.BeginArea(area);
 
-            switch (state)
-            {
-                case State.SelectMode: State_SelectMode(); break;
-                case State.SelectMap: State_SelectMap(); break;
-                case State.SelectRoom: State_SelectRoom(); break;
-                case State.StartClient: State_StartClient(); break;
-                case State.StartServer: State_StartServer(); break;
-            }
+			switch (state)
+			{
+				case State.SelectMode: State_SelectMode(); break;
+				case State.SelectMap: State_SelectMap(); break;
+				case State.SelectRoom: State_SelectRoom(); break;
+				case State.StartClient: State_StartClient(); break;
+				case State.StartServer: State_StartServer(); break;
+			}
 
-            GUILayout.EndArea();
-        }
+			GUILayout.EndArea();
+		}
 
-        void State_SelectRoom()
-        {
+		void State_SelectRoom()
+		{
 			GUI.Label(labelRoom, "Looking for rooms:", labelRoomStyle);
 
 			if (BoltNetwork.SessionList.Count > 0)
@@ -69,90 +69,90 @@ namespace Bolt.Samples
 				GUILayout.BeginVertical();
 				GUILayout.Space(30);
 
-		        foreach (var session in BoltNetwork.SessionList)
-		        {
-		            var photonSession = session.Value as PhotonSession;
+				foreach (var session in BoltNetwork.SessionList)
+				{
+					var photonSession = session.Value as PhotonSession;
 
-		            if (photonSession.Source == UdpSessionSource.Photon)
-		            {
-		                var matchName = photonSession.HostName;
-		                var label = string.Format("Join: {0} | {1}/{2}", matchName, photonSession.ConnectionsCurrent, photonSession.ConnectionsMax);
+					if (photonSession.Source == UdpSessionSource.Photon)
+					{
+						var matchName = photonSession.HostName;
+						var label = string.Format("Join: {0} | {1}/{2}", matchName, photonSession.ConnectionsCurrent, photonSession.ConnectionsMax);
 
-		                if (ExpandButton(label))
-		                {
-		                    BoltNetwork.Connect(photonSession);
-		                    state = State.Started;
-		                }
-		            }
-		        }
+						if (ExpandButton(label))
+						{
+							BoltNetwork.Connect(photonSession);
+							state = State.Started;
+						}
+					}
+				}
 
-                GUILayout.EndVertical();
+				GUILayout.EndVertical();
 			}
 		}
 
-        void State_SelectMode()
-        {
-            if (ExpandButton("Server"))
-            {
-                state = State.SelectMap;
-            }
-            if (ExpandButton("Client"))
-            {
-                state = State.StartClient;
-            }
+		void State_SelectMode()
+		{
+			if (ExpandButton("Server"))
+			{
+				state = State.SelectMap;
+			}
+			if (ExpandButton("Client"))
+			{
+				state = State.StartClient;
+			}
 		}
 
-        void State_SelectMap()
-        {
-            GUILayout.BeginVertical();
+		void State_SelectMap()
+		{
+			GUILayout.BeginVertical();
 
-            foreach (string value in BoltScenes.AllScenes)
-            {
-                if (SceneManager.GetActiveScene().name != value)
-                {
-                    if (ExpandButton(value))
-                    {
-                        map = value;
-                        state = State.StartServer;
-                    }
-                }
-            }
+			foreach (string value in BoltScenes.AllScenes)
+			{
+				if (SceneManager.GetActiveScene().name != value)
+				{
+					if (ExpandButton(value))
+					{
+						map = value;
+						state = State.StartServer;
+					}
+				}
+			}
 
-            GUILayout.EndVertical();
-        }
+			GUILayout.EndVertical();
+		}
 
-        void State_StartServer()
-        {
-            BoltLauncher.StartServer();
-            state = State.Started;
-        }
+		void State_StartServer()
+		{
+			BoltLauncher.StartServer();
+			state = State.Started;
+		}
 
-        void State_StartClient()
-        {
-            BoltLauncher.StartClient();
-            state = State.SelectRoom;
-        }
+		void State_StartClient()
+		{
+			BoltLauncher.StartClient();
+			state = State.SelectRoom;
+		}
 
-        public override void BoltStartDone()
-        {
-            if (BoltNetwork.IsServer)
-            {
-                var id = Guid.NewGuid().ToString().Split('-')[0];
-                var matchName = string.Format("{0} - {1}", id, map);
+		public override void BoltStartDone()
+		{
+			if (BoltNetwork.IsServer)
+			{
+				var id = Guid.NewGuid().ToString().Split('-')[0];
+				var matchName = string.Format("{0} - {1}", id, map);
 
-                BoltNetwork.SetServerInfo(matchName, null);
-                BoltNetwork.LoadScene(map);
-            }
-        }
+				BoltNetwork.SetServerInfo(matchName, null);
+				BoltNetwork.LoadScene(map);
+			}
+		}
 
-        bool ExpandButton(string text)
-        {
-            return GUILayout.Button(text, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
-        }
+		bool ExpandButton(string text)
+		{
+			return GUILayout.Button(text, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+		}
 
-        public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
-        {
-            BoltLog.Info("New session list");
-        }
-    }
+		public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+		{
+			BoltLog.Info("New session list");
+		}
+	}
 }
--- BoltInitSinglePlayer.cs
+++ BoltInitSinglePlayer.cs
@@ -6,85 +6,85 @@ using UnityEngine.SceneManagement;
 
 namespace Bolt.Samples
 {
-    public class BoltInitSinglePlayer : Bolt.GlobalEventListener
-    {
-        enum State
-        {
-            SelectMode,
-            SelectMap,
-            StartServer,
-            Started,
-        }
+	public class BoltInitSinglePlayer : Bolt.GlobalEventListener
+	{
+		enum State
+		{
+			SelectMode,
+			SelectMap,
+			StartServer,
+			Started,
+		}
 
-        State state;
-        string map;
+		State state;
+		string map;
 
-        void OnGUI()
-        {
-            Rect tex = new Rect(10, 10, 140, 75);
-            Rect area = new Rect(10, 90, Screen.width - 20, Screen.height - 100);
+		void OnGUI()
+		{
+			Rect tex = new Rect(10, 10, 140, 75);
+			Rect area = new Rect(10, 90, Screen.width - 20, Screen.height - 100);
 
-            GUI.Box(tex, Resources.Load("BoltLogo") as Texture2D);
-            GUILayout.BeginArea(area);
+			GUI.Box(tex, Resources.Load("BoltLogo") as Texture2D);
+			GUILayout.BeginArea(area);
 
-            switch (state)
-            {
-                case State.SelectMode: State_SelectMode(); break;
-                case State.SelectMap: State_SelectMap(); break;
-                case State.StartServer: State_StartSinglePlayer(); break;
-            }
+			switch (state)
+			{
+				case State.SelectMode: State_SelectMode(); break;
+				case State.SelectMap: State_SelectMap(); break;
+				case State.StartServer: State_StartSinglePlayer(); break;
+			}
 
-            GUILayout.EndArea();
-        }
+			GUILayout.EndArea();
+		}
 
-        void State_SelectMode()
-        {
-            if (ExpandButton("Start Single Player"))
-            {
-                state = State.SelectMap;
-            }
-        }
+		void State_SelectMode()
+		{
+			if (ExpandButton("Start Single Player"))
+			{
+				state = State.SelectMap;
+			}
+		}
 
-        void State_SelectMap()
-        {
-            GUILayout.BeginVertical();
+		void State_SelectMap()
+		{
+			GUILayout.BeginVertical();
 
-            foreach (string value in BoltScenes.AllScenes)
-            {
-                if (SceneManager.GetActiveScene().name != value)
-                {
-                    if (ExpandButton(value))
-                    {
-                        map = value;
-                        state = State.StartServer;
-                    }
-                }
-            }
+			foreach (string value in BoltScenes.AllScenes)
+			{
+				if (SceneManager.GetActiveScene().name != value)
+				{
+					if (ExpandButton(value))
+					{
+						map = value;
+						state = State.StartServer;
+					}
+				}
+			}
 
-            GUILayout.EndVertical();
-        }
+			GUILayout.EndVertical();
+		}
 
-        void State_StartSinglePlayer()
-        {
-            BoltLauncher.StartSinglePlayer();
-            state = State.Started;
-        }
+		void State_StartSinglePlayer()
+		{
+			BoltLauncher.StartSinglePlayer();
+			state = State.Started;
+		}
 
-        public override void BoltStartDone()
-        {
-            if (BoltNetwork.IsServer)
-            {
-                var id = Guid.NewGuid().ToString().Split('-')[0];
-                var matchName = string.Format("{0} - {1}", id, map);
+		public override void BoltStartDone()
+		{
+			if (BoltNetwork.IsServer)
+			{
+				var id = Guid.NewGuid().ToString().Split('-')[0];
+				var matchName = string.Format("{0} - {1}", id, map);
 
-                BoltNetwork.SetServerInfo(matchName, null);
-                BoltNetwork.LoadScene(map);
-            }
-        }
+				BoltNetwork.SetServerInfo(matchName, null);
+				BoltNetwork.LoadScene(map);
+			}
+		}
 
-        bool ExpandButton(string text)
-        {
-            return GUILayout.Button(text, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
-        }
-    }
+		bool ExpandButton(string text)
+		{
+			return GUILayout.Button(text, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+		}
+	}
 }

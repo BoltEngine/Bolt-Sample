8e284cf915d98d1475fcfa7924d941e3aaa79e47
Ramon Melo <me@ramonmelo.com>
2019-01-11 11:57:06 -0300

    ADDED: Client script to go back to Menu screen when Server shutdown

---
+++ GettingStarted/Scripts/ClientCallbacks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Bolt.Samples.GettingStarted
+{
+    [BoltGlobalBehaviour(BoltNetworkModes.Client, "Tutorial1")]
+    public class ClientCallbacks : Bolt.GlobalEventListener
+    {
+        public override void BoltShutdownBegin(AddCallback registerDoneCallback)
+        {
+            registerDoneCallback(() =>
+            {
+                Debug.Log("Shutdown Done");
+                SceneManager.LoadScene(0);
+            });
+        }
+    }
+}

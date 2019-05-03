0199cc99afe1ac45d5734e943b98db2d0d526554
Ramon Melo <me@ramonmelo.com>
2019-05-03 11:42:55 -0300

    ADDED: Override to BoltShutdown in order to load the Menu scene

--- GettingStarted/Scripts/ClientCallbacks.cs
+++ GettingStarted/Scripts/ClientCallbacks.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Bolt;
+using UdpKit;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Bolt.Samples.GettingStarted
 {
-    [BoltGlobalBehaviour(BoltNetworkModes.Client, "Tutorial1")]
-    public class ClientCallbacks : Bolt.GlobalEventListener
-    {
-        public override void BoltShutdownBegin(AddCallback registerDoneCallback)
-        {
-            registerDoneCallback(() =>
-            {
-                Debug.Log("Shutdown Done");
-                SceneManager.LoadScene(0);
-            });
-        }
-    }
+	[BoltGlobalBehaviour(BoltNetworkModes.Client, "Tutorial1")]
+	public class ClientCallbacks : Bolt.GlobalEventListener
+	{
+		public override void BoltShutdownBegin(AddCallback registerDoneCallback, UdpConnectionDisconnectReason disconnectReason)
+		{
+			registerDoneCallback(() =>
+			{
+				Debug.LogFormat("Shutdown Done with Reason: {0}", disconnectReason);
+				SceneManager.LoadScene(0);
+			});
+		}
+	}
 }
--- GettingStarted/Scripts/ServerCallbacks.cs
+++ GettingStarted/Scripts/ServerCallbacks.cs
@@ -1,27 +1,36 @@
-using UnityEngine;
 using System.Collections;
+using System.Linq;
 using Bolt.Tokens;
 using UdpKit;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Linq;
 
 namespace Bolt.Samples.GettingStarted
 {
-    [BoltGlobalBehaviour(BoltNetworkModes.Server, "Tutorial1")]
-    public class GS_ServerCallbacks : Bolt.GlobalEventListener
-    {
-        public override void Connected(BoltConnection connection)
-        {
-            var log = LogEvent.Create();
-            log.Message = string.Format("{0} connected", connection.RemoteEndPoint);
-            log.Send();
-        }
+	[BoltGlobalBehaviour(BoltNetworkModes.Server, "Tutorial1")]
+	public class GS_ServerCallbacks : Bolt.GlobalEventListener
+	{
+		public override void Connected(BoltConnection connection)
+		{
+			var log = LogEvent.Create();
+			log.Message = string.Format("{0} connected", connection.RemoteEndPoint);
+			log.Send();
+		}
 
-        public override void Disconnected(BoltConnection connection)
-        {
-            var log = LogEvent.Create();
-            log.Message = string.Format("{0} disconnected", connection.RemoteEndPoint);
-            log.Send();
-        }
-    }
+		public override void Disconnected(BoltConnection connection)
+		{
+			var log = LogEvent.Create();
+			log.Message = string.Format("{0} disconnected", connection.RemoteEndPoint);
+			log.Send();
+		}
+
+		public override void BoltShutdownBegin(AddCallback registerDoneCallback, UdpConnectionDisconnectReason disconnectReason)
+		{
+			registerDoneCallback(() =>
+			{
+				Debug.LogFormat("Shutdown Done with Reason: {0}", disconnectReason);
+				SceneManager.LoadScene(0);
+			});
+		}
+	}
 }

fb1c8309acd389cddba8ebefd501029b41a00ea8
Ramon Melo <me@ramonmelo.com>
2019-07-03 16:24:32 -0300

    CHANGED: Code formatting

--- GettingStarted/Scripts/Menu.cs
+++ GettingStarted/Scripts/Menu.cs
@@ -7,71 +7,68 @@ using UnityEngine;
 
 namespace Bolt.Samples.GettingStarted
 {
-    public class Menu : Bolt.GlobalEventListener
-    {
-        bool ShowGui = true;
+	public class Menu : Bolt.GlobalEventListener
+	{
+		bool ShowGui = true;
 
-        private void Awake()
-        {
-            Application.targetFrameRate = 60;
-            BoltLauncher.SetUdpPlatform(new PhotonPlatform());
-        }
+		private void Awake()
+		{
+			Application.targetFrameRate = 60;
+			BoltLauncher.SetUdpPlatform(new PhotonPlatform());
+		}
 
-        void OnGUI()
-        {
-            if (!ShowGui) { return; }
+		void OnGUI()
+		{
+			if (!ShowGui) { return; }
 
-            GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
+			GUILayout.BeginArea(new Rect(10, 10, Screen.width - 20, Screen.height - 20));
 
-            if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-            {
-                // START SERVER
-                BoltLauncher.StartServer();
-            }
+			if (GUILayout.Button("Start Server", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+			{
+				// START SERVER
+				BoltLauncher.StartServer();
+			}
 
-            if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
-            {
-                // START CLIENT
-                BoltLauncher.StartClient();
-            }
+			if (GUILayout.Button("Start Client", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
+			{
+				// START CLIENT
+				BoltLauncher.StartClient();
+			}
 
-            GUILayout.EndArea();
-        }
+			GUILayout.EndArea();
+		}
 
-        public override void BoltStartBegin()
-        {
-            ShowGui = false;
-        }
+		public override void BoltStartBegin()
+		{
+			ShowGui = false;
+		}
 
-        public override void BoltStartDone()
-        {
-            if (BoltNetwork.IsServer)
-            {
-                string matchName = Guid.NewGuid().ToString();
+		public override void BoltStartDone()
+		{
+			if (BoltNetwork.IsServer)
+			{
+				string matchName = Guid.NewGuid().ToString();
 
 				BoltMatchmaking.CreateSession(
-					sessionID: matchName, 
+					sessionID: matchName,
 					sceneToLoad: "Tutorial1"
 				);
+			}
+		}
 
-                // BoltNetwork.SetServerInfo(matchName, null);
-                // BoltNetwork.LoadScene("Tutorial1");
-            }
-        }
+		public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+		{
+			Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
 
-        public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
-        {
-            Debug.LogFormat("Session list updated: {0} total sessions", sessionList.Count);
+			foreach (var session in sessionList)
+			{
+				UdpSession photonSession = session.Value as UdpSession;
 
-            foreach (var session in sessionList)
-            {
-                UdpSession photonSession = session.Value as UdpSession;
-
-                if (photonSession.Source == UdpSessionSource.Photon)
-                {
-                    BoltNetwork.Connect(photonSession);
-                }
-            }
-        }
-    }
+				if (photonSession.Source == UdpSessionSource.Photon)
+				{
+					BoltNetwork.Connect(photonSession);
+				}
+			}
+		}
+	}
 }

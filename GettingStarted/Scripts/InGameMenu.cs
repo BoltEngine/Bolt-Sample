63427fc4b6bf1235a8af33d155f5938db5b90ff0
Ramon Melo <me@ramonmelo.com>
2020-01-14 09:08:11 -0300

    ADDED: Shutdown button to Getting Started sample

---
+++ GettingStarted/Scripts/InGameMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class InGameMenu : MonoBehaviour
+{
+	private Rect _shutdownButtonRect;
+
+	void Start()
+	{
+		_shutdownButtonRect = new Rect(Screen.width - 200, Screen.height - 100, 190, 60);
+	}
+
+	private void OnGUI()
+	{
+		if (GUI.Button(_shutdownButtonRect, "Shutdown") && BoltNetwork.IsRunning)
+		{
+			BoltNetwork.Shutdown();
+		}
+	}
+}

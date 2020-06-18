6187348a3d278c161390f62ef011585a828698c5
Ramon Melo <me@ramonmelo.com>
2020-06-18 11:01:27 -0300

    CHANGED: Removed Scene Entities by Spawners
    Now the scene entities from the Getting Started sample are created via script using a spawner instead of using Scene Objects directly

---
+++ GettingStarted/Scripts/InteractiveSpawner.cs
@@ -0,0 +1,23 @@
+﻿using Bolt;
+using UnityEngine;
+
+public class InteractiveSpawner : Bolt.GlobalEventListener
+{
+	[SerializeField] private BoltEntity prefabEntity;
+
+	private void Start()
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			transform.GetChild(i).gameObject.SetActive(false);
+		}
+	}
+
+	public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+	{
+		if (BoltNetwork.IsServer)
+		{
+			BoltNetwork.Instantiate(prefabEntity.gameObject, transform.position, Quaternion.identity);
+		}
+	}
+}

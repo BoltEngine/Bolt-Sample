c02995b3e0b6e99a29de752926f246d21e583c7d
Ramon Melo <me@ramonmelo.com>
2020-10-27 11:58:31 -0300

    ADDED: MoveAndShoot dedicated file to Tokens

--- SimpleMoveAndShoot/Scripts/Network/MoveAndShoot_Menu.cs
+++ SimpleMoveAndShoot/Scripts/Network/MoveAndShoot_Menu.cs
@@ -7,34 +7,6 @@ using UnityEngine.UI;
 
 namespace Bolt.Samples.MoveAndShoot
 {
-	public class HitInfo : PooledProtocolToken
-	{
-		public Vector3 hitPosition;
-		public bool hitType;
-
-		public override void Read(UdpPacket packet)
-		{
-			hitPosition.x = packet.ReadFloat();
-			hitPosition.y = packet.ReadFloat();
-			hitPosition.z = packet.ReadFloat();
-			hitType = packet.ReadBool();
-		}
-
-		public override void Write(UdpPacket packet)
-		{
-			packet.WriteFloat(hitPosition.x);
-			packet.WriteFloat(hitPosition.y);
-			packet.WriteFloat(hitPosition.z);
-			packet.WriteBool(hitType);
-		}
-
-		public override void Reset()
-		{
-			hitPosition = Vector3.zero;
-			hitType = false;
-		}
-	}
-
 	public class MoveAndShoot_Menu : Bolt.GlobalEventListener
 	{
 		// UI
---
+++ SimpleMoveAndShoot/Scripts/Network/MoveAndShoot_Tokens.cs
@@ -0,0 +1,33 @@
+﻿using UdpKit;
+using UnityEngine;
+
+namespace Bolt.Samples.MoveAndShoot
+{
+	public class HitInfo : PooledProtocolToken
+	{
+		public Vector3 hitPosition;
+		public bool hitType;
+
+		public override void Read(UdpPacket packet)
+		{
+			hitPosition.x = packet.ReadFloat();
+			hitPosition.y = packet.ReadFloat();
+			hitPosition.z = packet.ReadFloat();
+			hitType = packet.ReadBool();
+		}
+
+		public override void Write(UdpPacket packet)
+		{
+			packet.WriteFloat(hitPosition.x);
+			packet.WriteFloat(hitPosition.y);
+			packet.WriteFloat(hitPosition.z);
+			packet.WriteBool(hitType);
+		}
+
+		public override void Reset()
+		{
+			hitPosition = Vector3.zero;
+			hitType = false;
+		}
+	}
+}
